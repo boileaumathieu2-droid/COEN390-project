e859eb0b9ebbb14eee3d53753e8f5fef6b384ee1
neemabomediano <neemab001@gmail.com>
2026-07-16 18:01:55 -0400

    Create ObjectiveControler.java

---
+++ Zone/app/src/main/java/com/example/zone/model/ObjectiveControler.java
@@ -0,0 +1,8 @@
+package com.example.zone.controller;
+
+import com.example.zone.model.Database;
+
+public class ObjectiveController {
+    private Database database;
+    public ObjectiveController(Database database) {this.database = database;}
+}
