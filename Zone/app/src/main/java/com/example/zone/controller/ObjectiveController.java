a8e7e98a804cbbf15d3234c058faafd4dbe28d3e
Masry35 <johnmaged99@hotmail.com>
2026-07-19 17:56:52 -0400

    Update ObjectiveController.java

--- Zone/app/src/main/java/com/example/zone/controller/ObjectiveController.java
+++ Zone/app/src/main/java/com/example/zone/controller/ObjectiveController.java
@@ -1,28 +1,11 @@
 package com.example.zone.controller;
 
 import com.example.zone.model.Database;
-import com.example.zone.model.Objective;
-
-import java.util.ArrayList;
 
 public class ObjectiveController {
-    private Database database;
-    public ObjectiveController(Database database) {this.database = database;}
+    private final Database database;
 
-    public long addObjective(int userID, String text, String date){
-        return database.addObjective(userID, text, date);
+    public ObjectiveController(Database database) {
+        this.database = database;
     }
-
-    public boolean deleteObjective(int objectiveID){
-        return database.deleteObjective(objectiveID);
-    }
-
-    public ArrayList<Objective> getObjectives(int userID){
-        return database.getObjectives(userID);
-    }
-
-    public ArrayList<Objective> getObjectivesforDate(int userID, String date){
-        return database.getObjectivesForDate(userID, date);
-    }
-
 }
