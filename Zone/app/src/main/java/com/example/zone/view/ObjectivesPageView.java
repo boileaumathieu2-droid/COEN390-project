fc5ae4f7949dc391344c586acebd3d01faed1e1e
neemabomediano <neemab001@gmail.com>
2026-07-20 01:39:58 -0400

    Create ObjectivesPageView.java

---
+++ Zone/app/src/main/java/com/example/zone/view/ObjectivesPageView.java
@@ -0,0 +1,116 @@
+package com.example.zone.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.zone.R;
+import com.example.zone.controller.ObjectiveController;
+import com.example.zone.model.Database;
+import com.example.zone.model.Session;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class ObjectiveView extends AppCompatActivity {
+
+    private CalendarView calendar;
+    private Button objectiveButton;
+    private Button myObjectives;
+    private String date;
+    private ObjectiveController controller;
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_menu, menu);
+        return true;}
+    public boolean onOptionsItemSelected(MenuItem option) {
+        int id = option.getItemId();
+
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsView.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(option);
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.objective_view);
+        calendar = findViewById(R.id.objectiveCalendar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Set Objectives");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+
+        controller = new ObjectiveController(new Database(this));
+
+        date = new SimpleDateFormat(
+                "yyyy-MM-dd",
+                Locale.getDefault()
+        ).format(new Date());
+
+        objectiveButton = findViewById(R.id.newObjectiveButton);
+        myObjectives = findViewById(R.id.myObjectivesButton);
+
+        myObjectives.setOnClickListener(v-> {
+            Intent intent = new Intent(this, ObjectivesPageView.class);
+            startActivity(intent);
+
+        });
+
+        objectiveButton.setOnClickListener(v -> {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(ObjectiveView.this);
+
+            View popupView = getLayoutInflater().inflate(R.layout.create_objective, null);
+            builder.setView(popupView);
+
+            AlertDialog dialog = builder.create();
+
+            Button cancel = popupView.findViewById(R.id.btnCancel);
+            Button save = popupView.findViewById(R.id.btnSave);
+            EditText objectiveEdit = popupView.findViewById(R.id.objectiveEditText);
+
+            cancel.setOnClickListener(x -> dialog.dismiss());
+
+            save.setOnClickListener(x -> {
+                String objective = objectiveEdit.getText().toString();
+                controller.addObjective(Session.getUserID(), objective, date);
+                dialog.dismiss();
+            });
+
+            dialog.show();
+        });
+        calendar.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
+            date = String.format(
+                    Locale.getDefault(),
+                    "%04d-%02d-%02d",
+                    year,
+                    month + 1,
+                    dayOfMonth
+            );
+
+        });
+
+    }
+
+    }
