1bd770ce07a1d283e7893e9fdfc9f60367f03060
Tromboman22 <samuelpineau22@gmail.com>
2026-07-20 18:09:00 -0400

    Add average and resting heart rate fields to Analytics page

--- Zone/app/src/main/java/com/example/zone/view/AnalyticsView.java
+++ Zone/app/src/main/java/com/example/zone/view/AnalyticsView.java
@@ -2,15 +2,24 @@ package com.example.zone.view;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.zone.R;
 
 public class AnalyticsView extends AppCompatActivity {
 
+    private TextView heartRateValue;
+    private TextView avgHeartRateValue;
+    private TextView restingHeartRateValue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.analytics_page);
+
+        heartRateValue = findViewById(R.id.heartRateValue);
+        avgHeartRateValue = findViewById(R.id.avgHeartRateValue);
+        restingHeartRateValue = findViewById(R.id.restingHeartRateValue);
 
         Button mainMenuButton = findViewById(R.id.mainMenuButton);
         mainMenuButton.setOnClickListener(v -> finish());
