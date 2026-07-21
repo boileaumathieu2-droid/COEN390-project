948049c96fc818389a32b77548d89cbb99fe1d70
neemabomediano <neemab001@gmail.com>
2026-07-21 14:56:04 -0400

    Update and rename MainViewObjectiveAdapter to MainViewObjectiveAdapter.java

--- Zone/app/src/main/java/com/example/zone/model/MainViewObjectiveAdapter.java
+++ Zone/app/src/main/java/com/example/zone/model/MainViewObjectiveAdapter.java
@@ -1,3 +1,16 @@
+package com.example.zone.model;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.example.zone.R;
+
 import java.util.ArrayList;
 
 public class MainViewObjectiveAdapter extends ArrayAdapter<Objective> {
