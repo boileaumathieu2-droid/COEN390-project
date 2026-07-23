c52164f6610c2696560ef3ef5688b08c148f6ac9
Masry35 <johnmaged99@hotmail.com>
2026-07-22 20:28:27 -0400

    Update BlockApplication.java

--- Zone/app/src/main/java/com/example/zone/controller/BlockApplication.java
+++ Zone/app/src/main/java/com/example/zone/controller/BlockApplication.java
@@ -4,27 +4,14 @@ import android.accessibilityservice.AccessibilityService;
 import android.content.Intent;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
-import android.widget.Button;
 
 import com.example.zone.model.DetectBlockedApplication;
+import com.example.zone.model.BlockedAppsStore;
 import com.example.zone.model.StudySessionModel;
-
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 public class BlockApplication extends AccessibilityService {
     private StudySessionModel Session = StudySessionModel.getInstance();
 
-    private final Set<String> blockedApps = new HashSet<>(Arrays.asList(
-            "com.instagram.android",
-            "com.google.android.youtube",
-            "com.zhiliaoapp.musically",
-            "com.reddit.frontpage",
-            "com.facebook.katana",
-            "com.snapchat.android",
-            "com.google.android.gm"
-    ));
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
 
@@ -33,7 +20,7 @@ public class BlockApplication extends AccessibilityService {
         if (Session.getStatus() == StudySessionModel.Status.ACTIVE) {
             String packageName = event.getPackageName().toString();
             Log.d("APP_BLOCKER", "Opened app: " + packageName);
-            if (blockedApps.contains(packageName)) {
+            if (BlockedAppsStore.isBlocked(this, packageName)) {
                 Log.d("it recognizes the package", packageName);
                 Intent intent = new Intent(
                         this,
@@ -47,8 +34,4 @@ public class BlockApplication extends AccessibilityService {
     @Override
     public void onInterrupt() {
     }
-    public void manageRestrictedApps(){
-    }
 }
-
-
