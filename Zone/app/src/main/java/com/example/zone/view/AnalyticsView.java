87f22d514f486fcca58af59c25ea339ea90cf00a
neemabomediano <neemab001@gmail.com>
2026-08-04 22:44:00 -0400

    added back button

--- Zone/app/src/main/java/com/example/zone/view/AnalyticsView.java
+++ Zone/app/src/main/java/com/example/zone/view/AnalyticsView.java
@@ -46,6 +46,26 @@ public class AnalyticsView extends AppCompatActivity {
         }
     };
 
+        @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem option) {
+        if (option.getItemId() == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsView.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(option);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
