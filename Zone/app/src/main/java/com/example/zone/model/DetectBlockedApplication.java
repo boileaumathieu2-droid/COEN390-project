22ac62f411b4ebc233e811fba4c74bbaf7fce4e1
neemabomediano <neemab001@gmail.com>
2026-08-05 23:31:45 -0400

    fixed a bug that causes the app to crash when attempting to return to Zone

--- Zone/app/src/main/java/com/example/zone/model/DetectBlockedApplication.java
+++ Zone/app/src/main/java/com/example/zone/model/DetectBlockedApplication.java
@@ -21,8 +21,11 @@ public class DetectBlockedApplication extends AppCompatActivity {
         button = findViewById(R.id.button);
 
         button.setOnClickListener(v -> {
-            Intent intent = new Intent(this, MainView.class);
+            Intent intent = new Intent(this, MainContainerActivity.class);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
+            finish();
 
         });
     }
